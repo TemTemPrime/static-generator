b78f230e444051ff4e87693135554464ed7a96b1
TemTemPrime <ephraimplayz@gmail.com>
2026-05-30 18:16:50 +0100

    weird

--- src/gencontent.py
+++ src/gencontent.py
@@ -14,7 +14,7 @@ def generate_page(from_path, template_path, dest_path, basepath):
     title = extract_title(markdown_content)
     template_content = template_content.replace("{{ Title }}", title)
     template_content = template_content.replace("{{ Content }}", html_string)
-    template_content = template_content.replace( 'href="/ ', basepath )
+    template_content = template_content.replace('href="/', basepath )
     template_content = template_content.replace('src="/',basepath)
     dir_name = os.path.dirname(dest_path)
     os.makedirs(dir_name, exist_ok=True)
